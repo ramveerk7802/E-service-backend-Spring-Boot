586c8d211f9674f150b637b05ea1a6d5aa48d01b
Ramveer <ramveerk7802@gmail.com>
2025-04-10 11:00:50 +0530

    Service type Annotate

--- src/main/java/com/rvcode/E_service/repositories/ServiceTypeRepository.java
+++ src/main/java/com/rvcode/E_service/repositories/ServiceTypeRepository.java
@@ -2,9 +2,11 @@ package com.rvcode.E_service.repositories;
 
 import com.rvcode.E_service.entities.ServiceType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ServiceTypeRepository extends JpaRepository<ServiceType,Long> {
     Optional<ServiceType> findByServiceName(String serviceName);
 }
